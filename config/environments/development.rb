9491c05403684a1fec490a16fd4845a25c65717c
Trevor Kurschner <trevor.kurschner@gmail.com>
2014-02-19 06:18:50 -0500

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,13 +28,13 @@ LearnRails::Application.configure do
   config.assets.debug = true
 
 config.action_mailer.smtp_settings = {
-address: "smtp.gmail.com",
-port: 587,
-domain: ENV["DOMAIN_NAME"],
-authentication: "plain",
-enable_starttls_auto: true,
-user_name: ENV["GMAIL_USERNAME"],
-password: ENV["GMAIL_PASSWORD"]
+	address: "smtp.gmail.com",
+	port: 587,
+	domain: ENV["DOMAIN_NAME"],
+	authentication: "plain",
+	enable_starttls_auto: true,
+	user_name: ENV["GMAIL_USERNAME"],
+	password: ENV["GMAIL_PASSWORD"]
 }
 
 # Send email in development mode.
